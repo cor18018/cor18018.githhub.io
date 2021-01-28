66d5a2216d140e9fdbb71a3a5b386c85cf4e5f13
cor18018 <cor18018@byui.edu>
2021-01-28 09:29:00 -0700

    nav

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,4 +3,6 @@ window.addEventListener("load", ()=> {
     const mainnav = document.querySelector("#navigation");
 
     hambutton.addEventListener("click", ()=> {mainnav.classList.toggle("resonsive")},false);
+
+    window.onresize = ()=> {if(window.innerWidth>760) mainnav.classList.remove("responsive");};
 });
