2b36eb5c77afd4dcd5ba32f83cf2b5d966d333af
cor18018 <cor18018@byui.edu>
2021-02-03 15:17:06 -0700

    lesson5

---
+++ lesson5/js/page.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", ()=>{
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    const d = new Date();
+    var today = new Date();
+
+    today = days[d.getDay()] + ", " + String(today.getDate()).padStart(2, "0") + " " + monthNames[d.getMonth()] 
+    + " " + today.getFullYear();
+
+    const lu = document.querySelector("#currentDate");
+    lu.textContent = today;
+
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+
+    const menubutton = document.querySelector(".menu");
+    const mainnav = document.querySelector("#nav");
+
+    menubutton.addEventListener("click", ()=> {mainnav.classList.toggle("resonsive")},false);
+
+    window.onresize = ()=> {if(window.innerWidth>760) mainnav.classList.remove("responsive");};
+    
+});
