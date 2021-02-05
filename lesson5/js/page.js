07796a28b86b4a0034d45b8fc8453c5e9cedebb1
cor18018 <cor18018@byui.edu>
2021-02-04 17:48:59 -0700

    remove message

--- lesson5/js/page.js
+++ lesson5/js/page.js
@@ -10,17 +10,23 @@ window.addEventListener("load", ()=>{
     today = days[d.getDay()] + ", " + String(today.getDate()).padStart(2, "0") + " " + monthNames[d.getMonth()] 
     + " " + today.getFullYear();
 
-    const lu = document.querySelector("#currentDate");
-    lu.textContent = today;
+    const current_date = document.querySelector("#currentDate");
+    current_date.textContent = today;
 
     const cry = document.querySelector("#copyrightyear");
     cry.textContent = new Date().getFullYear();
 
     const menubutton = document.querySelector(".menu");
     const mainnav = document.querySelector("#nav");
-
     menubutton.addEventListener("click", ()=> {mainnav.classList.toggle("resonsive")},false);
-
     window.onresize = ()=> {if(window.innerWidth>760) mainnav.classList.remove("responsive");};
     
+    const message = document.querySelector(".alert_message");
+    var friday = d.getDate();
+    if (friday != 5) {
+        message.classList.toggle("no_alert");
+    }
+    else {
+        message.classList.remove("no_alert");
+    }
 });
