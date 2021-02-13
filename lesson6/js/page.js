fef31d628f611f6cedbb0220cb5d496252879d07
cor18018 <cor18018@byui.edu>
2021-02-13 15:41:54 -0700

    5 day forecast

--- lesson6/js/page.js
+++ lesson6/js/page.js
@@ -1,14 +1,15 @@
-window.addEventListener("load", ()=>{
+window.addEventListener("load", () => {
     const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"];
+        "July", "August", "September", "October", "November", "December"
+    ];
 
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
+
     const d = new Date();
     var today = new Date();
 
-    today = days[d.getDay()] + ", " + String(today.getDate()).padStart(2, "0") + " " + monthNames[d.getMonth()] 
-    + " " + today.getFullYear();
+    today = days[d.getDay()] + ", " + String(today.getDate()).padStart(2, "0") + " " + monthNames[d.getMonth()] +
+        " " + today.getFullYear();
 
     const current_date = document.querySelector("#currentDate");
     current_date.textContent = today;
@@ -18,15 +19,27 @@ window.addEventListener("load", ()=>{
 
     const menubutton = document.querySelector(".menu");
     const mainnav = document.querySelector("#nav");
-    menubutton.addEventListener("click", ()=> {mainnav.classList.toggle("resonsive")},false);
-    window.onresize = ()=> {if(window.innerWidth>760) mainnav.classList.remove("responsive");};
-    
+    menubutton.addEventListener("click", () => {
+        mainnav.classList.toggle("resonsive")
+    }, false);
+    window.onresize = () => {
+        if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+    };
+
     const message = document.querySelector(".alert_message");
     var friday = d.getDate();
     if (friday != 5) {
         message.classList.toggle("no_alert");
-    }
-    else {
+    } else {
         message.classList.remove("no_alert");
     }
+    ! function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://weatherwidget.io/js/widget.min.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }(document, 'script', 'weatherwidget-io-js');
 });
