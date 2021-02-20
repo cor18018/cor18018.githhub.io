125c8a0cd8a7e47b7130712175f51a915bd97d5e
cor18018 <cor18018@byui.edu>
2021-02-19 21:41:44 -0700

    gallery

--- lesson7/js/page.js
+++ lesson7/js/page.js
@@ -28,4 +28,31 @@ window.addEventListener("load", (event) => {
     images.forEach(image=>{
         imgObserver.observe(image);
     })
+
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+    ];
+
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const d = new Date();
+    var today = new Date();
+
+    today = days[d.getDay()] + ", " + String(today.getDate()).padStart(2, "0") + " " + monthNames[d.getMonth()] +
+        " " + today.getFullYear();
+
+    const current_date = document.querySelector("#currentDate");
+    current_date.textContent = today;
+
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+
+    const menubutton = document.querySelector(".menu");
+    const mainnav = document.querySelector("#nav");
+    menubutton.addEventListener("click", () => {
+        mainnav.classList.toggle("resonsive")
+    }, false);
+    window.onresize = () => {
+        if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+    };
 });
