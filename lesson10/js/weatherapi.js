01af1a7232018a177f9a04b93f2fcc49d0e053bc
cor18018 <cor18018@byui.edu>
2021-03-11 09:23:42 -0700

    Weather App
    Co-authored-by: Eddy Gonzalez <oswaldo4495@gmail.com>

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,14 +1,36 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=70a3382c326cf2cbb5e5cc5922554860&units=imperial';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5605242&appid=70a3382c326cf2cbb5e5cc5922554860&units=imperial';
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
 
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
-    const desc = jsObject.weather[0].description;  
-    document.getElementById('imagesrc').textContent = imagesrc;  
-    document.getElementById('icon').setAttribute('src', imagesrc);
-    document.getElementById('icon').setAttribute('alt', desc);
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const d = new Date();
+    var today = new Date();
+
+    today = days[d.getDay()]
+
+    document.getElementById('current-temp').textContent = Math.round(jsObject.main.temp);
+    document.getElementById('today').textContent = today;
+    document.getElementById('status').textContent = jsObject.weather[0].main;   
+
+
+
+    // let five_days = document.getElementById('day');
+    // five_days = document.createElement('p');
+  });
+
+  const apiURL2 = 'https://api.openweathermap.org/data/2.5/forecast?id=5605242&appid=70a3382c326cf2cbb5e5cc5922554860&units=imperial'
+  fetch(apiURL2)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    document.getElementById('friday').innerHTML = Math.round(jsObject.list[7].main.temp) + "&deg;";
+    document.getElementById('saturday').innerHTML = Math.round(jsObject.list[15].main.temp) + "&deg;";
+    document.getElementById('sunday').innerHTML = Math.round(jsObject.list[23].main.temp) + "&deg;";
+    document.getElementById('monday').innerHTML = Math.round(jsObject.list[31].main.temp) + "&deg;";
+    document.getElementById('tuesday').innerHTML = Math.round(jsObject.list[39].main.temp) + "&deg;";
+
   });
