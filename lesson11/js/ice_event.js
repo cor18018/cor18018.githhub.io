deafd4d6ffa0e8ca65e0aeca486dc1d0cea6e713
cor18018 <cor18018@byui.edu>
2021-03-18 09:23:55 -0600

    ice event

--- lesson11/js/ice_event.js
+++ lesson11/js/ice_event.js
@@ -1,11 +1,12 @@
 window.addEventListener("load", () => {
-    const requestURL = 'http://www.ahfx.com/events.php'
+    const requestURL = 'https://www.ahfx.com/events.php'
     fetch(requestURL)
         .then((response) => {
             return response.json();
         })
         .then((jsonObject) => {
             console.log(jsonObject);
+            let counter = 1;   // CM ADDED THIS
             Object.entries(jsonObject).forEach(([key,event])=> {
                 buildEventCard(event);
             });
@@ -23,12 +24,13 @@ function buildEventCard(event){
     for (var i = 0; i < event.tags.length; i++){
                             
         }
-    card.innerHTML = `<p>${event.tags[0]}</p>
+    card.innerHTML = `<div class="box-${counter}">
+                    <p>${event.tags[0]}</p>
                      <h2>${event.properties.name}</h2>
                      <p>Location: ${event.properties.location}</p>
                      <p>Event Time: ${start_time}</p>
                      <p>Event Date: ${start_date}</p>
                      <p><em>Event organized by: ${event.properties.organized_by} </em></p>
-                     <p><em>Event type: ${event.type}</em></p>`;
+                     <p><em>Event type: ${event.type}</em></p></div>`;
     document.querySelector(".card").appendChild(card);
 }
