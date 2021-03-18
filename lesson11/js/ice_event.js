c3ff1a6fd0231636285d0797ab639da6f7467274
cor18018 <cor18018@byui.edu>
2021-03-18 09:49:29 -0600

    ice event

--- lesson11/js/ice_event.js
+++ lesson11/js/ice_event.js
@@ -6,14 +6,12 @@ window.addEventListener("load", () => {
         })
         .then((jsonObject) => {
             console.log(jsonObject);
-            let counter = 1;   // CM ADDED THIS NOT SURE IT WOULD WORK
             Object.entries(jsonObject).forEach(([key,event])=> {
                 buildEventCard(event);
-                counter++;
             });
         });
 });
-function buildEventCard(event){
+function buildEventCard(event,){
     let card = document.createElement("section");
     card.classList.add("event");
     let start = event.properties.start;
@@ -22,16 +20,12 @@ function buildEventCard(event){
     let end = event.properties.end;
     end_time = end.substr(end.indexOf(' ')+1);
     end_date = end.substr(0,end.indexOf(' '))
-    for (var i = 0; i < event.tags.length; i++){
-                            
-        }
-    card.innerHTML = `
-                    <p>${event.tags[0]}</p>
+    card.innerHTML = `<p>${event.tags[0]}</p>
                      <h2>${event.properties.name}</h2>
                      <p>Location: ${event.properties.location}</p>
                      <p>Event Time: ${start_time}</p>
                      <p>Event Date: ${start_date}</p>
                      <p><em>Event organized by: ${event.properties.organized_by} </em></p>
                      <p><em>Event type: ${event.type}</em></p></div>`;
-    document.querySelector(".card").appendChild(card);
+    document.querySelector('.card').appendChild(card);
 }
