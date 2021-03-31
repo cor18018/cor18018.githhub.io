738133dbb50af05fb0ca9af080ec82f2f35dc18b
cor18018 <cor18018@byui.edu>
2021-03-31 12:08:12 -0600

    pages

---
+++ final/js/contact.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", (event) => {
+
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+
+    const menubutton = document.querySelector(".menu");
+    const mainnav = document.querySelector("#nav");
+    menubutton.addEventListener("click", () => {
+        mainnav.classList.toggle("resonsive")
+    }, false);
+    window.onresize = () => {
+        if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+    };
+    
+});
