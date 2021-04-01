3627f6b827c79216848f1c1646a509a384e29b9f
cor18018 <cor18018@byui.edu>
2021-04-01 11:39:05 -0600

    Reservations

--- final/js/temples.js
+++ final/js/temples.js
@@ -24,40 +24,62 @@ window.addEventListener("load", (event) => {
                 buildTempleCard(temple);
             });
         });
+    
+    function buildTempleCard(temple){
+
+        let card = document.createElement("section");
+        let heading1 = document.createElement("p");
+        let heading2 = document.createElement("p");
+        let heading3 = document.createElement("p");
+        let heading4 = document.createElement("p");
+        let weather = document.createElement("div");
+        let current = document.createElement("span");
+        let high = document.createElement("span");
+        let city = temple.zip;
+        const apiURL = 'https://api.openweathermap.org/data/2.5/weather?zip='+city+'&appid=70a3382c326cf2cbb5e5cc5922554860&units=imperial';
+        card.classList.add("temple");
+        card.innerHTML = `<h3>${temple.name}</h3>
+                            <img src="${temple.imageurl}" alt="${temple.name}">
+                            <p>Contact Info: </p>
+                            <p>Address: ${temple.address1} ${temple.city}, ${temple.state} ${temple.zip}</p>
+                            <p>Phone Number: <a href="#">${temple.phone}</a></p>
+                            <p>Email: <a href="#">${temple.email}</a></p>
+                            <p>Status: ${temple.status}</p>
+                            <p>Services: </p>`;
+        
+        for(var i = 0; i < temple.services.length; i++){
+            let service = document.createElement("p");
+            service.textContent = temple.services[i];
+            card.appendChild(service);
+        }
+        heading1.textContent = "Closures: ";
+        card.appendChild(heading1);
+        for(var i = 0; i < temple.closures.length; i++){
+            let closures = document.createElement("p");
+            closures.textContent = temple.closures[i];
+            card.appendChild(closures);
+        }
+        heading2.textContent = "History: ";
+        card.appendChild(heading2);
+        for(var i = 0; i < temple.history.length; i++){
+            let history = document.createElement("p");
+            history.textContent = temple.history[i];
+            card.appendChild(history);
+        }
+        fetch(apiURL)
+        .then((response) => response.json())
+        .then((jsObject) => {
+            console.log(jsObject);
+            heading3.textContent = "Current Weather: ";
+            weather.appendChild(heading3);
+            current.textContent = Math.round(jsObject.main.temp) + " °F";
+            heading3.appendChild(current);
+            heading4.textContent = "High: ";
+            weather.appendChild(heading4);
+            high.textContent = Math.round(jsObject.main.temp_max) + " °F";
+            heading4.appendChild(high);
+        });
+        document.querySelector(".card").appendChild(card);
+        card.appendChild(weather);
+    }
 });
-function buildTempleCard(temple){
-    
-    let card = document.createElement("section");
-    let heading1 = document.createElement("p");
-    let heading2 = document.createElement("p");
-    card.classList.add("temple");
-    card.innerHTML = `<h3>${temple.name}</h3>
-                     <img src="${temple.imageurl}" alt="${temple.name}">
-                     <p>Contact Info: </p>
-                     <p>Address: ${temple.address1} ${temple.city}, ${temple.state} ${temple.zip}</p>
-                     <p>Phone Number: <a href="#">${temple.phone}</a></p>
-                     <p>Email: <a href="#">${temple.email}</a></p>
-                     <p>Status: ${temple.status}</p>
-                     <p>Services: </p>`;
-    
-    for(var i = 0; i < temple.services.length; i++){
-        let service = document.createElement("p");
-        service.textContent = temple.services[i];
-        card.appendChild(service);
-    }
-    heading1.textContent = "Closures: ";
-    card.appendChild(heading1);
-    for(var i = 0; i < temple.closures.length; i++){
-        let closures = document.createElement("p");
-        closures.textContent = temple.closures[i];
-        card.appendChild(closures);
-    }
-    heading2.textContent = "History: ";
-    card.appendChild(heading2);
-    for(var i = 0; i < temple.history.length; i++){
-        let history = document.createElement("p");
-        history.textContent = temple.history[i];
-        card.appendChild(history);
-    }
-    document.querySelector(".card").appendChild(card);
-}
