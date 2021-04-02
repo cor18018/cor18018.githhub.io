298df5b91ab8239a771d0bf1f77797519abfcd70
cor18018 <cor18018@byui.edu>
2021-04-02 11:43:32 -0600

    Sub pages services

--- final/js/services.js
+++ final/js/services.js
@@ -5,39 +5,13 @@ window.addEventListener("load", (event) => {
 
     const menubutton = document.querySelector(".menu");
     const mainnav = document.querySelector("#nav");
+    const services = document.querySelector("#services");
     menubutton.addEventListener("click", () => {
         mainnav.classList.toggle("resonsive")
+        services.classList.toggle("resonsive")
     }, false);
     window.onresize = () => {
         if (window.innerWidth > 760) mainnav.classList.remove("responsive");
+        else if (window.innerWidth > 760) services.classList.remove("responsive");
     };
-    
-    /*API*/
-    const requestURL = 'js/temples.json'
-    fetch(requestURL)
-        .then((response) => {
-            return response.json();
-        })
-        .then((templeImages) => {
-            console.log(templeImages);
-            
-        })
-        // .then((jsonObject) => {
-        //     console.log(jsonObject);
-        //     Object.entries(jsonObject).forEach(([key,temple])=> {
-        //         buildTempleCard(temple);
-        //     });
-        // });
 });
-// function buildTempleCard(temple){
-//     let card = document.createElement("section");
-//     card.classList.add("temple");
-//     card.innerHTML = `<h2>${temple.name}</h2>
-//                      <img src="${temple.imageurl}" alt="${temple.name}">
-//                      <p>Address: ${temple.address1} ${temple.city}, ${temple.state} ${temple.zip}</p>
-//                      <p>Contact Info: ${temple.phone}</p>
-//                      <p>Status: Open</p>
-//                      <p>First President: ${temple.presidents[0]} 1st of ${temple.presidents.length}</p>
-//                      <p>Current President: ${temple.presidents[temple.presidents.length-1]}</p>`;
-//     document.querySelector("#temples").appendChild(card);
-// }
